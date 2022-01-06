ce6be7fadf275f49b3af165d5af331741b6df2de
GoldenbyteGH <48603774+GoldenbyteGH@users.noreply.github.com>
2022-01-06 18:57:29 +0100

    Update TurnOffVm.py

--- TurnOffVm.py
+++ TurnOffVm.py
@@ -67,9 +67,9 @@ if __name__ == '__main__':
         print("ERROR - missing  parameter")
         exit()
     gbserver = ESXI_Srv(config["Account"]["IP"],config["Account"]["user"], config["Account"]["esxi_en_psw"], config["Account"]["key"])
-    GNS3_ID = gbserver.get_world_id(arg[0])
-    if GNS3_ID == -1:
+    VM_ID = gbserver.get_world_id(arg[0])
+    if VM_ID == -1:
         raise ValueError(arg[0]+' vm not found')
 
-    output = gbserver.turn_off_vm(GNS3_ID)
+    output = gbserver.turn_off_vm(VM_ID)
     print("VM "+arg[0]+" turned OFF\n")
